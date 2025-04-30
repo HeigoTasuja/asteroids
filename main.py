0e5e1190b98fd5f9fa5e61683fd8eb4b6ade02df
HeigoTasuja <heigo.tasuja@gmail.com>
2025-04-30 22:58:45 +0300

    player

--- main.py
+++ main.py
@@ -2,20 +2,26 @@ import pygame
 
 # internal imports
 from constants import *
+from player import Player
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     running = True
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
 
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
 
-    screen.fill((0, 0, 0))
+    screen.fill(("black"))
+    player.draw(screen)
     pygame.display.flip()
+    dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
