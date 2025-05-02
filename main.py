d4d5c88042bb25a05f00d8b536f0420f730acbad
HeigoTasuja <heigo.tasuja@gmail.com>
2025-05-02 22:02:43 +0300

    add player turns

--- main.py
+++ main.py
@@ -8,20 +8,20 @@ from player import Player
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    running = True
     clock = pygame.time.Clock()
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
 
-    while running:
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                running = False
+                return
 
-    screen.fill(("black"))
-    player.draw(screen)
-    pygame.display.flip()
-    dt = clock.tick(60) / 1000
+        player.update(dt)
+        screen.fill(("black"))
+        player.draw(screen)
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
