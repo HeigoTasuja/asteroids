645cecbbe85cb96f0b533056d5fbabac27377711
HeigoTasuja <heigo.tasuja@gmail.com>
2025-05-05 22:10:15 +0300

    add collision

--- main.py
+++ main.py
@@ -30,6 +30,12 @@ def main():
                 return
 
         updatable.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                print("Game over!")
+                sys.exit()
+
         screen.fill(("black"))
 
         for obj in drawable:
